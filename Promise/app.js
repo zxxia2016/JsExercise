990f49f45c86991417abab093e42a1a2a628fbb4
zxxia2016 <zxxia2016@gmail.com>
2022-03-07 15:04:35 +0800

    Promise Execise

--- Promise/app.js
+++ Promise/app.js
@@ -3,7 +3,7 @@ console.log(`start app`);
 
 new Promise((resolve, reject) => {
     setTimeout(() => {
-        console.log(`dfadddf`);
+        console.log(`timeout`);
         //如果没调用resolve或reject函数是不会执行then函数的
         resolve();
     }, 5000);
@@ -11,4 +11,15 @@ new Promise((resolve, reject) => {
     console.log(`then`);
 })
 console.log(`end app`);
+
+/**
+start app
+Promise/app.js:2
+end app
+Promise/app.js:13
+timeout
+Promise/app.js:6
+then
+Promise/app.js:11
+ */
 
