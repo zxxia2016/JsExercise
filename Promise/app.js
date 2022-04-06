990f9b1a8d31c7c30a45cfb1c345540e553cbcd6
zxxia2016 <zxxia2016@gmail.com>
2022-04-06 09:38:26 +0800

    Promise 异步队列

--- Promise/app.js
+++ Promise/app.js
@@ -1,6 +1,7 @@
 
 console.log(`start app`);
 
+// start app >> end app >> timeout >> then
 new Promise((resolve, reject) => {
     setTimeout(() => {
         console.log(`timeout`);
@@ -10,16 +11,51 @@ new Promise((resolve, reject) => {
 }).then(() => {
     console.log(`then`);
 })
+
+// 执行异步队列任务
+
+let f1 = function () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`f1`);
+            resolve();
+        }, 2000);
+    });
+};
+
+let f2 = function () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`f2`);
+            resolve();
+        }, 1000);
+    });
+};
+
+let f3 = function () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`f3`);
+            resolve();
+        }, 3000);
+    });
+};
+
+const plist = [f1, f2, f3];
+// 递归调用
+// 使用 await & async
+async function promise_queue(list) {
+    let index = 0
+    while (index >= 0 && index < list.length) {
+        await list[index]();
+        index++
+    }
+}
+promise_queue(plist);
+
 console.log(`end app`);
 
-/**
-start app
-Promise/app.js:2
-end app
-Promise/app.js:13
-timeout
-Promise/app.js:6
-then
-Promise/app.js:11
- */
+
+
+
 
